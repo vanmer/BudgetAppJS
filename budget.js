217986e84a65eede68ad837ad4af6f81732878dd
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-25 15:08:16 +0200

    select elements

--- budget.js
+++ budget.js
@@ -1,1 +1,10 @@
-/* www.youtube.com/CodeExplained */
+// select elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
