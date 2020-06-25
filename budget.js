fbc09adade54675a120f3a666a041d5fa64fcece
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-25 22:35:07 +0200

    select buttons

--- budget.js
+++ budget.js
@@ -8,3 +8,10 @@ const allEl = document.querySelector("#all");
 const incomeList = document.querySelector("#income .list");
 const expenseList = document.querySelector("#expense .list");
 const allList = document.querySelector("#all .list");
+
+// select buttons
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// input buttons
