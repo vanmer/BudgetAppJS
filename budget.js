f921c73d15419f1a4f1aa9d02df4f1939c27abab
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-26 12:20:15 +0200

    drawCircle, update chart

--- budget.js
+++ budget.js
@@ -106,6 +106,8 @@ function updateUI() {
     }
     showEntry(allList, entry.type, entry.title, entry.amount, index)
   });
+
+  updateChart(income, outcome);
 }
 
 function showEntry( list, type, title, amount, id) {
--- chart.js
+++ chart.js
@@ -13,7 +13,24 @@ chart.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
 // change the line width
-ctx.lineWidth = 8;
+ctx.lineWidth = 7;
 
 // circle radius
 const R = 20;
+
+// draw circle
+function drawCircle(color, ratio, anticlockwise) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.arc( canvas.width/2, canvas.height/2, R, 0, ratio * 2 * Math.PI, anticlockwise);
+  ctx.stroke();
+}
+
+// update chart
+function updateChart(income, outcome) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  let ratio = income / (income + outcome);
+
+  drawCircle("#FFF", 1 - ratio, true);
+  drawCircle("#F0624D", 1 - ratio, false);
+}
